0dbc50be87c7822e383e7a3a072b2f14db215524
Roman Handke <roman.handke@online.de>
2018-08-08 14:01:52 +0200

    Added Column in employees table

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -4,7 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Employee::class, function (Faker $faker) {
     return [
-      'first_name'  => $faker->firstName,
-      'last_name'   => $faker->lastName
+      'first_name'    => $faker->firstName,
+      'last_name'     => $faker->lastName,
+      'vacation_days' => $faker->numberBetween(26,30)
     ];
 });
--- database/migrations/2018_08_01_100743_create_employees_table.php
+++ database/migrations/2018_08_01_100743_create_employees_table.php
@@ -17,6 +17,7 @@ class CreateEmployeesTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->integer('vacation_days');
             $table->timestamps();
         });
     }
